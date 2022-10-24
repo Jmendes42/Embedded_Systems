09ab47a8fb467cb91ca17f33c4f9ad2a5b3df86e
joao-mendes723 <joao.brandao.mendes@gmail.com>
2022-10-24 03:25:07 +0100

    blinky

---
+++ Registers/blinkBinary/blink.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+#define BLINK_MS 200
+
+//#define MASK(x) ((unsigned char) (1<<(x))) = _BV()
+
+int main() {
+
+    DDRD = 0xFF;	// Set DDRD to 0b11111111
+	PORTB = _BV(0);	// Enable pullup resistor on pin0
+
+    while (true) {
+		if (!(PINB & _BV(0))) {		// Check if button is pushed
+	        PORTD += 1 << 0; 		// Increment binary count
+			_delay_ms(BLINK_MS);
+		}
+    }
+}
