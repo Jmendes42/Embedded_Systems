b850c3773e08c53342e02145489a893486b07a02
joao-mendes723 <joao.brandao.mendes@gmail.com>
2022-10-24 03:35:40 +0100

    blinkyBin

--- Registers/blinkBinary/blink.c
+++ Registers/blinkBinary/blink.c
@@ -7,9 +7,8 @@
 int main() {
 
 	DDRD = 0xFF;	// Set DDRD to 0b11111111
-	PORTB = _BV(0);	// Enable pullup resistor on pin0
-
-    while (true) {
+	PORTB = _BV(0);	// Enable pullup resistor on p
+	while (true) {
 		if (!(PINB & _BV(0))) {		// Check if button is pushed
 			PORTD += 1 << 0; 		// Increment binary count
 			_delay_ms(BLINK_MS);
